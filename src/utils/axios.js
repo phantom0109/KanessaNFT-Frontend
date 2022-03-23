ef0b616b8477aa73f77ea354326b3121e6373e8d
phantom0109 <hayatekimura0109@gmail.com>
2022-03-23 16:48:44 -0400

    demo

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-import { DEBUG } from "../config/config";
-
-axios.defaults.baseURL = DEBUG ? "http://localhost:8000/api" : "/api";
+var env = process.env.NODE_ENV || 'development';
+axios.defaults.baseURL = env === 'development' ? "http://localhost:8000/api" : "/api";
 
 export default axios;
