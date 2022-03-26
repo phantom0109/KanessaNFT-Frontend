fd23aa1b666c973d948a9c9f88072bfdd68142c9
phantom0109 <hayatekimura0109@gmail.com>
2022-03-26 12:36:15 -0400

    change icon, clean code

--- backend/src/routes/api.route.js
+++ backend/src/routes/api.route.js
@@ -2,9 +2,6 @@ const { apiController } = require("../controllers");
 const Router = require("express").Router;
 const router = new Router();
 
-router.get("/metadata/:id", apiController.getMetadata);
-router.get("/tokens", apiController.getBurgers);
-
 router.get("/whitelist/:address", apiController.getWhitelist);
 
 router.get("/whitelist", apiController.getWhitelistRoot);
