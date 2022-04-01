3372bca0b5072dd2a5949f9f2b01d9504ad7fda1
phantom0109 <hayatekimura0109@gmail.com>
2022-04-01 14:56:07 -0400

    complete

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ export const DEBUG = true;
 export const MAINNET_RPC_URL = "https://mainnet.infura.io/v3/5a99373559f8468eb4d40e15b3dd9812";
 export const TESTNET_RPC_URL = "https://rinkeby.infura.io/v3/5a99373559f8468eb4d40e15b3dd9812";
 
-export const contractAddress = "0xD40AD87F86919058558661B2105DAbF4248305e0";
+export const contractAddress = "0xF637860c509e804C640C13c720d4150D5b3AC457";
 
 export const NETWORK_ID = DEBUG ? 4 : 1;
 export const RPC_URL = NETWORK_ID == 4 ? TESTNET_RPC_URL : MAINNET_RPC_URL;
